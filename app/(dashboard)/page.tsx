7afeee41a8892267d99dd86be11cb326a637a4af
Eddy Rivera Echegaray <eddyriveraechegaray@Eddys-MBP.homenet.telecomitalia.it>
2024-01-16 18:59:43 +0100

    Create form

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -18,7 +18,9 @@ function Home() {
       <Separator className="my-6" />
       <h2 className="cold-span-2 text-4xl font-bold">Your forms</h2>
       <Separator className="my-6" />
-      <CreateFormBtn />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <CreateFormBtn />
+      </div>
     </div>
   );
 }
